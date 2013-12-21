53e8d23cc4d563560cbce85491856fc4eef7bbda
Atanas Korchev <akorchev@gmail.com>
2013-12-21 14:43:11 +0200

    Update grid-webapi-crud to use the Kendo.DynamicLinq NuGet package.

--- grid-webapi-crud/App_Code/DataSourceRequest.cs
+++ grid-webapi-crud/App_Code/DataSourceRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Kendo.DynamicLinq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- grid-webapi-crud/App_Code/ProductsController.cs
+++ grid-webapi-crud/App_Code/ProductsController.cs
@@ -8,6 +8,7 @@ using System.Net.Http;
 using System.Web.Http;
 using NorthwindModel;
 using Newtonsoft.Json;
+using Kendo.DynamicLinq;
 
 public class ProductsController : ApiController
 {
