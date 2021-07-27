1bd898e7215fb93218330ca4d6e53eb05732d050
Georgi Denchev <gdenchev@progress.com>
2021-07-27 17:14:28 +0300

    chore: add webapi with core example

---
+++ grid-core-webapi-crud/ApiJqueryGrid/Controllers/GridController.cs
@@ -0,0 +1,59 @@
+﻿using ApiJqueryGrid.Models;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApiJqueryGrid.Controllers
+{
+    [Route("api/[controller]")]
+    public class GridController : ControllerBase
+    {
+        private static List<OrderViewModel> orders;
+
+        [HttpGet("Read")]
+        public DataSourceResult GetOrders([DataSourceRequest] DataSourceRequest request)
+        {
+            if (orders == null)
+            {
+                orders = Enumerable.Range(1, 50).Select(i => new OrderViewModel
+                {
+                    OrderID = i,
+                    Category = new Category() { CategoryID = 2, CategoryName = "Delivered" },
+                    OrderDate = new DateTime(2016, 9, 15).AddDays(i % 7),
+                    ShipName = "ShipName " + i,
+                    ShipCity = "ShipCity " + i
+                }).ToList();
+            }
+
+
+            return orders.ToDataSourceResult(request);
+        }
+
+        [HttpPost("Create")]
+        public IActionResult Create(OrderViewModel order)
+        {
+            order.OrderID = orders.Count + 1;
+            orders.Add(order);
+
+            return new ObjectResult(new DataSourceResult { Data = new[] { order }, Total = 1 });
+        }
+
+        [HttpPut("Update")]
+        public IActionResult Update(OrderViewModel order)
+        {
+
+            return new StatusCodeResult(200);
+        }
+
+        [HttpDelete("Destroy")]
+        public IActionResult Destroy(OrderViewModel order)
+        {
+            orders.Remove(order);
+
+            return new StatusCodeResult(200);
+        }
+    }
+}
---
+++ grid-core-webapi-crud/ApiJqueryGrid/Startup.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiJqueryGrid
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services
+                .AddControllersWithViews()
+                // Maintain property names during serialization. See:
+                // https://github.com/aspnet/Announcements/issues/194
+                .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+
+            // Add Kendo UI services to the services container
+            services.AddKendo();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
