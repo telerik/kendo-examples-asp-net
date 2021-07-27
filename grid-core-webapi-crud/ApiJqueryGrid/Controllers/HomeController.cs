7a9a979918d7835f8f3ca2040377252db85ce8a0
Georgi Denchev <gdenchev@progress.com>
2021-07-27 17:14:28 +0300

    chore: add webapi with core example

---
+++ grid-core-webapi-crud/ApiJqueryGrid/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiJqueryGrid.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Angular()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ grid-core-webapi-crud/ApiJqueryGrid/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiJqueryGrid.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+
+        public string CategoryName { get; set; }
+    }
+}
