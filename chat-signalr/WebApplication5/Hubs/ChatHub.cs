92b4c32fac42188ac6fa83737d69b8abc52726ed
martintabakov <Martin.Tabakov@progress.com>
2023-02-17 10:35:24 +0200

    add peer-to-peer chat

---
+++ chat-signalr/WebApplication5/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(object sender, string message)
+        {
+            // Broadcast the message to all clients except the sender.
+            await Clients.Others.SendAsync("broadcastMessage", sender, message);
+        }
+
+        public async Task SendTyping(object sender)
+        {
+            // Broadcast the typing notification to all clients except the sender.
+            await Clients.Others.SendAsync("typing", sender);
+        }
+    }
+}
